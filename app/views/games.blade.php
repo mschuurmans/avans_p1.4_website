74dff8c9cdac0f8bcc4027f767a7ff7572d2187f
Michiel Schuurmans <michiel@codox.org>
2014-05-27 12:56:52 +0200

    changes

--- app/views/games.blade.php
+++ app/views/games.blade.php
@@ -1,1 +1,10 @@
-games
+@extends('layouts.default')
+@section('content')
+<b>Current games:</b> <br />
+Indiana Jantje <br />
+Flappy Bird <br />
+Fruit Opps<br />
+Whac a Fardoes<br />
+Simon Loves Fruit<br />
+Red Button 
+@endsection
