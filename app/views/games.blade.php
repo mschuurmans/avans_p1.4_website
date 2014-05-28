95574d6a7f4816b575a21113c62b5b09910ceb4c
Michiel Schuurmans <michiel@codox.org>
2014-05-28 09:18:42 +0200

    added splashscreens of games

--- app/views/games.blade.php
+++ app/views/games.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.default')
 @section('content')
 <b>Current games:</b> <br />
-Indiana Jantje <br />
+{{ HTML::image('images/indianajantje_splashscreen.png')}}<br />
 Flappy Bird <br />
-Fruit Opps<br />
-Whac a Fardoes<br />
+{{HTML::image('images/fruitops_splashscreen.png')}}<br />
+{{HTML::image('images/whacafardoes_splashscreen.png')}}<br />
 Simon Loves Fruit<br />
 Red Button 
 @endsection
