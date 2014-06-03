75a752a46284bf6c1edd700700895acc44773b4f
Michiel Schuurmans <michiel@codox.org>
2014-06-03 13:51:33 +0200

    added numers by highscores

--- app/views/highscores.blade.php
+++ app/views/highscores.blade.php
@@ -6,15 +6,18 @@
 		text-align: left;
 	}
 </style>
+<?php $count = 1; ?>
 <table style="width: 100%;">
 	<tbody>
-		<tr style="width: 940px; border: 1px solid black;" class="thead"><th style="min-width: 30%">Name</th><th style="width: 30%" class="thead">Score</th><th style="width: 30%" class="thead">Date</th></tr>
+		<tr style="width: 940px; border: 1px solid black;" class="thead"><th class="thead">nr</th><th style="min-width: 30%">Name</th><th style="width: 30%" class="thead">Score</th><th style="width: 30%" class="thead">Date</th></tr>
 		@foreach ($scores as $score)
     			<tr>
+				<td><?php echo $count; ?></td>
 				<td>{{ $score->name }}</td>
 				<td>{{ $score->score }}</td>
 				<td>{{ $score->date }}</td>	
 			</tr>
+			<?php $count++; ?>
 		@endforeach
 	</tbody>
 </table>
