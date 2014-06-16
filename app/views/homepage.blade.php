1def0f2164bd0ceaaca38ac508640a08e77e59d0
The Essperience <essperience@cbox.nl>
2014-06-16 15:04:33 +0200

    added movie on homepage

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -6,4 +6,7 @@
 Dit is de website van "The Essperience". Een school project van de 4e periode projectgroep A5.<Br />
 Op deze site staan alle minigames omschreven en kun je de source code vinden. Ook hebben we een live stream<br /> 
 voor tijdens presentaties en eventueel open dagen. 
+<br /><br />
+
+<iframe width="560" height="315" src="//www.youtube.com/embed/Lj3zuH8j-rY" frameborder="0" allowfullscreen></iframe>
 @endsection
