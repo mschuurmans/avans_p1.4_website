bbd66fa65cae118d8132b3e340febd5b3b48fcd9
Michiel Schuurmans <michiel@codox.org>
2014-06-16 11:38:07 +0200

    edited homepage

--- app/views/homepage.blade.php
+++ app/views/homepage.blade.php
@@ -2,9 +2,8 @@
 @section('header')
   header here <br /> 
 @endsection
-@section('footer')
-  footer
-@endsection
 @section('content')
-  content
+Dit is de website van "The Essperience". Een school project van de 4e periode projectgroep A5.<Br />
+Op deze site staan alle minigames omschreven en kun je de source code vinden. Ook hebben we een live stream<br /> 
+voor tijdens presentaties en eventueel open dagen. 
 @endsection
